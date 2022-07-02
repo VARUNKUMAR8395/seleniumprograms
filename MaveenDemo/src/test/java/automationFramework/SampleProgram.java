08ed7d5fb6961eea63abaffb66ad250b81231c1b
varun <varun@DESKTOP-0G3UNK2>
2022-07-02 20:16:47 +0530

    first commit

---
+++ MaveenDemo/src/test/java/automationFramework/SampleProgram.java
@@ -0,0 +1,12 @@
+package automationFramework;
+
+public class SampleProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("today is sunday");
+	}
+
+}
